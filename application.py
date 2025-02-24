10341ade8bfdecd1fea470c4a1ee4ecdcadccef0
swag mode activate!!!! <nicholas.kennedy2@ucalgary.ca>
2025-02-23 17:15:00 -0700

    api stuff all done, only need to update readme now

--- application.py
+++ application.py
@@ -112,8 +112,8 @@ def bookpage(book_id):
         average_rating = "no ratings found on google books api."    
 
     try:
-        bookapidesc = json["items"][0]['volumeInfo']["description"]
-    except:
+        bookapidesc = json["items"][0]["volumeInfo"]["description"]
+    except (KeyError, IndexError):
         bookapidesc = "no description found on google books api."
  
     #gemini api fetch, the rawdata is pasted from lab page
@@ -130,7 +130,6 @@ def bookpage(book_id):
     gemini_json = (gemini_res.json())
     gemini_json = gemini_json["candidates"][0]["content"]["parts"][0]["text"]
     
-
     if request.method == "GET":
         reviews = db.execute(text("SELECT * FROM reviews WHERE book_id = :id"), {"id":book_id}).fetchall()
         return render_template("bookpage.html", book=book, username=session["username"], reviews=reviews, average_rating=average_rating, number_ratings=number_ratings, gemini_json=gemini_json)
@@ -150,6 +149,76 @@ def bookpage(book_id):
 
         reviews = db.execute(text("SELECT * FROM reviews WHERE book_id = :id"), {"id":book_id}).fetchall()
         return render_template("bookpage.html", book=book, username=session["username"], reviews=reviews, message=message, average_rating=average_rating, number_ratings=number_ratings, gemini_json=gemini_json)
+
+@app.route("/api/<string:isbn>", methods=["GET"])
+def api(isbn):
+    #initialize book and get google api reviews
+    book = db.execute(text("SELECT * FROM books WHERE isbn = :isbn"), {"isbn":isbn}).fetchone()
+
+    if book:
+        #Get GEMINI google api key from enviornment varibales
+        gemini_key = os.environ["GEMINI_KEY"]
+
+        #rest of info needs google api
+        res = requests.get("https://www.googleapis.com/books/v1/volumes", params={"q": f"isbn:{isbn}"})
+        json = (res.json())
+        #check if theres any ratings
+        try:
+            number_ratings = json["items"][0]["volumeInfo"]["ratingsCount"]
+            average_rating = json["items"][0]["volumeInfo"]["averageRating"]
+        except (KeyError, IndexError):
+            number_ratings = "Null"
+            average_rating = "Null"   
+        try:
+            bookapidesc = json["items"][0]["volumeInfo"]["description"]
+        except (KeyError, IndexError):
+            bookapidesc = "Null"
+        try:
+            pubdate = json["items"][0]["volumeInfo"]["publishedDate"]
+        except (KeyError, IndexError):
+            pubdate = "Null"
+        #getting the isbns
+        isbn_10 = "Null"
+        isbn_13 = "Null"
+        for identifier in json["items"][0]["volumeInfo"]["industryIdentifiers"]:
+            if identifier["type"] == "ISBN_10":
+                isbn_10 = identifier["identifier"]
+            if identifier["type"] == "ISBN_13":
+                isbn_13 = identifier["identifier"]
+        #this way they will be null if the loop doesnt find them
+        
+        #gemini api fetch, the rawdata is pasted from lab page
+        rawdata = {
+            "contents": [{
+                "parts": [
+                    {
+                        "text": f"Summarize this text using less than 50 words: {bookapidesc}"
+                    }
+                ]
+            }]
+        }
+        gemini_res = requests.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent", params={"key":gemini_key}, headers={"Content-Type":"application/json"}, json=rawdata)
+        gemini_json = (gemini_res.json())
+        gemini_desc = gemini_json["candidates"][0]["content"]["parts"][0]["text"]
+
+        #make the dictionary and return it
+        bookdict = {
+            "title":book.title,
+            "author":book.author,
+            "publishedDate":pubdate,
+            "ISBN_10":isbn_10,
+            "ISBN_13":isbn_13,
+            "reviewCount":number_ratings,
+            "averageRating":average_rating,
+            "description":bookapidesc,
+            "summarizedDescription":gemini_desc
+        }
+        return bookdict
+
+    else:
+        error404 = {"code":404}
+        return error404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
